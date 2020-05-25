d1eaff50c339332b37bb9a1835b66ee290e23cde
simran-sr <simran@spiderorb.in>
2020-05-25 15:57:40 +0530

    migrations changes

--- onboarding/apps/employee/views/login.py
+++ onboarding/apps/employee/views/login.py
@@ -1,7 +1,18 @@
+import uuid
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django.http import HttpResponse
+from onboarding.apps.employee.models.employee import ModelEmployee
 
 # Create your views here.
 class ViewAutoLoginOnboard(LoginView):
 	def __init__(self):
 		print("selff...")
+		greeting = "Good Day"
+	
+	def get(self, request, id):
+		print(self.kwargs['id'])
+		id = self.kwargs['id']
+		employee = ModelEmployee.objects.get(id=id)
+		print("employeee == ", employee)
+		return HttpResponse(self.greeting)
--- onboarding/settings/environment_development.py
+++ onboarding/settings/environment_development.py
@@ -5,7 +5,7 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'onboarding.sqlite3'),
         # 'USER': '',
         # 'PASSWORD': '',
         # 'HOST': '127.0.0.1',
